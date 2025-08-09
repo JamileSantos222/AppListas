1d1aad2fc28fb88ab72c76235948b406814a6f08
JamileSantos222 <jamile.alexandre.santos.222@gmail.com>
2025-08-08 21:26:17 -0300

    adaptador

---
+++ app/src/main/java/br/com/etecia/myapp/AdaptadorProdutos.java
@@ -0,0 +1,9 @@
+package br.com.etecia.myapp;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdaptadorProdutos extends RecyclerView.Adapter<> {
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+    }
+}
